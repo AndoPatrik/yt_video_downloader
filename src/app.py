f51b4b3592843dba741d56217632a3b73793f279
AndoPatrik <screadern@gmail.com>
2022-07-30 17:50:19 +0200

    Add main page with input and download button

---
+++ src/app.py
@@ -0,0 +1,36 @@
+from distutils import text_file
+import tkinter as tk
+from turtle import clear
+from PIL import Image, ImageTk
+
+# GUI start point
+root = tk.Tk()
+
+# Grid setup
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=4)
+
+# Logo
+logo = Image.open('../static/logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
+# YT Link input
+link_entry_text = tk.StringVar()
+link_entry = tk.Entry(root, textvariable=link_entry_text, width=80)
+link_entry.grid(columnspan=3, column=0, row=1)
+
+# Insturctions
+instructions = tk.Label(root, text="Paste YouTube link and click on download.")
+instructions.grid(columnspan=3, column=0, row=2)
+
+# Download butotn
+download_btn_text = tk.StringVar()
+download_btn = tk.Button(root, textvariable=download_btn_text)
+download_btn_text.set("Download")
+download_btn.grid(column=1, row=3)
+
+# End of GUI
+root.mainloop()
