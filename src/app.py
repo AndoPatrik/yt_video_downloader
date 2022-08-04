fbad67af221744142e74057472c9a7e0468f5bcb
AndoPatrik <screadern@gmail.com>
2022-08-04 17:38:07 +0200

    Swap tkinter to customtkinter

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import customtkinter as ctk
 import settings_window
 import download
 from PIL import Image, ImageTk
@@ -18,41 +18,46 @@ def settings_button_click():
     settings_window.display(root)
 
 
-# GUI start point
-root = tk.Tk()
+# General settings
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("../static/custom_theme.json")
 
-# Grid setup
-canvas = tk.Canvas(root, width=600, height=300)
-canvas.grid(columnspan=3, rowspan=4)
+# GUI start point
+root = ctk.CTk()
+root.grid_rowconfigure(4, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
 # Logo
 logo = Image.open('../static/logo.png')
 logo = ImageTk.PhotoImage(logo)
-logo_label = tk.Label(image=logo)
+logo_label = ctk.CTkLabel(image=logo)
 logo_label.image = logo
-logo_label.grid(column=1, row=0)
+logo_label.grid(column=0, row=0, columnspan=3)
 
 # YT Link input
-link_entry_text = tk.StringVar()
-link_entry = tk.Entry(root, textvariable=link_entry_text, width=80)
+link_entry_text = ctk.StringVar()
+link_entry = ctk.CTkEntry(
+    root, textvariable=link_entry_text, width=300)
 link_entry.grid(columnspan=3, column=0, row=1)
 
 # Insturctions
-instructions = tk.Label(root, text="Paste YouTube link and click on download.")
+instructions = ctk.CTkLabel(
+    root, text="Paste YouTube link and click on download.")
 instructions.grid(columnspan=3, column=0, row=2)
 
 # Download button
-download_btn_text = tk.StringVar()
-download_btn = tk.Button(root,
-                         textvariable=download_btn_text,
-                         command=lambda: download_button_click())
+download_btn_text = ctk.StringVar()
+download_btn = ctk.CTkButton(root,
+                             textvariable=download_btn_text,
+                             command=lambda: download_button_click())
 download_btn_text.set("Download")
-download_btn.grid(column=1, row=3)
+download_btn.grid(column=0, row=3, columnspan=3)
 
 # Settings button
-settings_button = tk.Button(root, text='Settings',
-                            command=lambda: settings_button_click())
-settings_button.grid(column=2, row=3)
+settings_button = ctk.CTkButton(root, text='Settings',
+                                command=lambda: settings_button_click())
+settings_button.grid(column=0, row=4, columnspan=3)
 
 # Run the GUI main loop
 root.mainloop()
+í
--- src/settings_window.py
+++ src/settings_window.py
@@ -1,7 +1,7 @@
-import tkinter as tk
+import customtkinter as ctk
 
 
 def display(root):
-    settings_window = tk.Toplevel(root)
+    settings_window = ctk.CTkToplevel(root)
     settings_window.title = "Settings"
     settings_window.geometry = "200x200"
