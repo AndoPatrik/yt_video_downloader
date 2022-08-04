61ba1249c1c2eefc48fa38793bf9aacb8ec39c15
AndoPatrik <screadern@gmail.com>
2022-08-04 19:06:36 +0200

    Adding radio buttons to define download type

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from tkinter.tix import COLUMN
 import customtkinter as ctk
 import download
 from PIL import Image, ImageTk
@@ -13,32 +14,53 @@ def download_button_click():
         print("Error: Missing YT link")
 
 
+def radiobutton_event():
+    print("radiobutton toggled, current value:", settings_radio_var.get())
+
+
 # General settings
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("../static/custom_theme.json")
 
 # GUI start point
 root = ctk.CTk()
+root.title("YT video downloader")
+root.geometry("400x500")
+root.resizable(False, False)
 root.grid_rowconfigure(4, weight=1)
-root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
 
 # Logo
 logo = Image.open('../static/logo.png')
 logo = ImageTk.PhotoImage(logo)
 logo_label = ctk.CTkLabel(image=logo)
 logo_label.image = logo
-logo_label.grid(column=0, row=0, columnspan=3, pady=20)
+logo_label.grid(column=0, row=0, columnspan=4, pady=20)
+
+# Settings radio buttons
+settings_radio_var = ctk.IntVar()
+
+settings_radiobutton_audio = ctk.CTkRadioButton(master=root, text="Only audio",
+                                                command=radiobutton_event, variable=settings_radio_var, value=1)
+settings_radiobutton_video = ctk.CTkRadioButton(master=root, text="Only video",
+                                                command=radiobutton_event, variable=settings_radio_var, value=2)
+settings_radiobutton_both = ctk.CTkRadioButton(master=root, text="Video and audio",
+                                               command=radiobutton_event, variable=settings_radio_var, value=3)
+
+settings_radiobutton_audio.grid(column=0, row=1, padx=10)
+settings_radiobutton_video.grid(column=1, row=1, padx=10)
+settings_radiobutton_both.grid(column=2, row=1, padx=10)
 
 # YT Link input
 link_entry_text = ctk.StringVar()
 link_entry = ctk.CTkEntry(
     root, textvariable=link_entry_text, width=300)
-link_entry.grid(columnspan=3, column=0, row=1, pady=20)
+link_entry.grid(columnspan=4, column=0, row=2, pady=20)
 
 # Insturctions
 instructions = ctk.CTkLabel(
     root, text="Paste YouTube link and click on download.")
-instructions.grid(columnspan=3, column=0, row=2)
+instructions.grid(columnspan=4, column=0, row=3)
 
 # Download button
 download_btn_text = ctk.StringVar()
@@ -46,7 +68,10 @@ download_btn = ctk.CTkButton(root,
                              textvariable=download_btn_text,
                              command=lambda: download_button_click())
 download_btn_text.set("Download")
-download_btn.grid(column=0, row=3, columnspan=3, pady=20)
+download_btn.grid(column=0, row=4, columnspan=4, pady=20)
+
+# Config elements
+settings_radiobutton_both.select()
 
 # Run the GUI main loop
 root.mainloop()
